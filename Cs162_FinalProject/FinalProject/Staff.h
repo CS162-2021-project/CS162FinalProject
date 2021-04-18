967805e83cc1b11709dd65fd645269d174def35a
Nhu-Huynh <mannhuch@gmail.com>
2021-04-18 18:54:37 +0700

    view list of students in course

--- Cs162_FinalProject/FinalProject/Staff.h
+++ Cs162_FinalProject/FinalProject/Staff.h
@@ -1,6 +1,10 @@
 #ifndef _STAFF_H_
 #define _STAFF_H_
+
 void viewListOfCLasses(Year* pYear);
 
 void viewListOfStudentsInClass(Year* pYear, Class* pClass, char* className);
+
+void viewListOfStudentsInCourse (Student *pStuInCourse);
+
 #endif // !_STAFF_H_
---
+++ Cs162_FinalProject/FinalProject/ViewListOfStudentInCourse.cpp
@@ -0,0 +1,13 @@
+#include<string.h>
+#include "Staff.h"
+#include "Header.h"
+#include<iostream>
+
+using namespace std;
+
+void viewListOfStudentsInCourse (Student *pStuInCourse) {
+    while (pStuInCourse) {
+        cout << pStuInCourse->studentID << "\t" << pStuInCourse->Name << "\t" << pStuInCourse->DOB.day << "/" << pStuInCourse->DOB.month << "/"  <<  pStuInCourse->DOB.year << "\t" << pStuInCourse->sClass << "\t" << pStuInCourse->gender << endl;
+        pStuInCourse = pStuInCourse->pStuInCourse.studenNext;
+    }
+}
