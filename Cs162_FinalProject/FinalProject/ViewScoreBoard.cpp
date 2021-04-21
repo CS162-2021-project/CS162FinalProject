8ffa68faa8c621de89b55cb5fe614d8adff7957d
dehjaza2001 <81430050+dehjaza2001@users.noreply.github.com>
2021-04-21 14:52:23 +0700

    viewScoreBoard(update)

--- Cs162_FinalProject/FinalProject/ViewScoreBoard.cpp
+++ Cs162_FinalProject/FinalProject/ViewScoreBoard.cpp
@@ -3,6 +3,22 @@
 #include<iostream>
 using namespace std;
 
-void viewScoreBoard(Course pCourse, char* studentID) {
-
+void viewScoreBoard(Course* pCourse, char* studentID) {
+	while (pCourse) {
+		while (pCourse->pScoreboard) {
+			if (strcmp(pCourse->pScoreboard->stu.studentID, studentID) == 0) {
+				cout << "Final score :";
+				cout << pCourse->pScoreboard->final << endl;
+				cout << "Midterm score :";
+				cout << pCourse->pScoreboard->midterm << endl;
+				cout << "Bonus :";
+				cout << pCourse->pScoreboard->bonus;
+				cout << "Total score :";
+				cout << pCourse->pScoreboard->total << endl;
+				return;
+			}
+			pCourse->pScoreboard = pCourse->pScoreboard->scoreboardNext;
+		}
+		pCourse = pCourse->courseNext;
+	}
 }
