f1cf93ffa526cb7a0425261db5959a9ee30d4175
Nhu-Huynh <mannhuch@gmail.com>
2021-05-01 13:38:33 +0700

    Update check.cpp

--- Cs162_FinalProject/FinalProject/check.cpp
+++ Cs162_FinalProject/FinalProject/check.cpp
@@ -3,17 +3,13 @@
 #include "Header.h"
 #include "Staff.h"
 
-void findScore (Scoreboard * & pScr, char* stuID){
-    while (pScr->stu && pScr->stu->studentID != stuID) {
-        pScr->stu = pScr->stu->studentNext;
+int findHighestMidScore (Scoreboard * & pScr){
+    int max = 0;
+    while (pScr) {
+        if (pScr->midterm > max) {
+            max = pScr->midterm;
+        }
+        pScr = pScr->scoreboardNext;
     }
-
-    if(pScr->stu != nullptr){
-        cout << "Midterm: " << pScr->midterm << endl;
-        cout << "Final: " << pScr->final << endl;
-        cout << "Bonus: " << pScr->bonus << endl;
-        cout << "Total: " << pScr->total << endl;
-    }else{
-        cout << "Student ID not found." << endl;
-    }
+    return max;
 }
